8d6fe886725f95a96d80ae028654c2ac6f29ee9d
Nahid Hossain <mdnahidislam019321@gmail.com>
2026-09-23 15:20:29 +0600

    Fix cPanel Laravel deployment and view paths

--- public/index.php
+++ public/index.php
@@ -1,15 +1,16 @@
 <?php
+
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+if (file_exists($maintenance = __DIR__.'/../Fol-Bazar-Laravel/storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__.'/../Fol-Bazar-Laravel/vendor/autoload.php';
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__.'/../Fol-Bazar-Laravel/bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
